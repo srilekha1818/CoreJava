340ad10c0f41d9603a47c71b9f443b54ea623db5
srilekha1818 <srilekhakavuri18@gmail.com>
2023-05-11 15:16:20 +0530

    commit message

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 11-05-2023
+method overloading: multiple methods with same name but differ in maybe the type of parameters,number of parameters in the same class can be known as method overloading.
---
+++ method_overloading/MethodOl.java
@@ -0,0 +1,31 @@
+class Sri{
+ 
+int add(int a,int b,int c){
+return a+b+c;
+ }
+
+
+float add(int a,float b)
+{
+    return a+b;
+}
+
+double add(int a,double b)
+{
+    return a+b;
+}
+ 
+}
+public class MethodOl{
+    public static void main (String[] args){
+        Sri sri1=new Sri();
+        int a=12;int b=4;
+        double m=13.456;
+        float n=12.34f;
+        System.out.println(sri1.add(a,n));
+        System.out.println(sri1.add(a,m));
+        System.out.println(sri1.add(a,b));
+
+
+    }
+}
