87f48e3fb0675537fa9a6362968a89239c467cf4
srilekha1818 <srilekhakavuri18@gmail.com>
2023-05-27 14:53:53 +0530

    committed encapsulation and this

---
+++ Encapsulation/DBConnect.java
@@ -0,0 +1,14 @@
+class DbConnect{
+    public DbConnect(){
+        System.out.println("connected to database");
+    }
+}
+
+
+
+public class DBConnect{
+    public static void main(String[] args){
+DbConnect db =new DbConnect();
+
+    }
+}
---
+++ Encapsulation/LaunchDog.java
@@ -0,0 +1,30 @@
+class Dog{
+private String name;
+private int cost;
+
+public void setName(String name) {
+    this.name = name;
+}
+public int getCost() {
+    return cost;
+}
+public void setCost(int cost) {
+    this.cost = cost;
+}
+public String getName() {
+    return name;
+}
+
+}
+
+
+public class LaunchDog {
+    public static void main(String[] args){
+Dog d=new Dog();
+d.setName("Sheru");
+System.out.println(d.getName());
+d.setCost(2000);
+System.out.println(d.getCost());
+
+    }
+}
