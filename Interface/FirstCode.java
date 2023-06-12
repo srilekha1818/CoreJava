dbdd48e208d420ef8f8474e525bb1bfdad6c26d0
srilekha1818 <srilekhakavuri18@gmail.com>
2023-06-12 21:47:54 +0530

    commit

---
+++ Interface/FirstCode.java
@@ -0,0 +1,28 @@
+package Interface;
+interface ISample{
+    void disp();
+    void show();
+    
+}
+abstract class SampleImpl implements ISample{
+    public void disp(){
+        System.out.println("disp is implementing in sampleimpl");
+    }
+}
+class Sample extends SampleImpl{
+    public void show(){
+        System.out.println("show is extending in sample");
+    }
+}
+
+
+public class FirstCode {
+public  static void main(String[] args) {
+ISample smp=new Sample();
+smp.disp();
+smp.show();
+
+    
+}
+    
+}
