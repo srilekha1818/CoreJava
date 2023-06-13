b81dea8a71f7c2f2f413ded370174a6579f78d28
srilekha1818 <srilekhakavuri18@gmail.com>
2023-06-13 17:20:52 +0530

    commit

---
+++ Interface/Interface/InterMethodNaming.java
@@ -0,0 +1,34 @@
+package Interface;
+interface one{
+    void disp();
+}
+interface two{
+   // void disp();//one method implementation is enough
+   //int disp(); //we cannot implement two interfaces simultaneously as return type of the methods are different
+  void disp(int i);
+}
+class InterMethod implements one,two{
+
+    @Override
+    public void disp(int i) {
+        System.out.println("disp with arg"); 
+    }
+
+    @Override
+    public void disp() {
+        System.out.println("disp with no args");
+       
+        
+    }
+
+}
+public class InterMethodNaming{
+
+
+public static void main(String[] args) {
+    InterMethod im =new InterMethod();
+    im.disp();
+    im.disp(12);
+            
+}          
+}
---
+++ Interface/Interface/ThirdCode.java
@@ -0,0 +1,14 @@
+package Interface;
+//variables
+interface bluetooth{
+    boolean wireless=true;
+}
+public class ThirdCode implements bluetooth {
+    public static void main(String[] args){
+        //wireless=false; cannot assign a value to final variable
+        boolean wireless=false; //local variable
+        System.out.println(bluetooth.wireless);
+        System.out.println(wireless);//false
+    }
+    
+}
