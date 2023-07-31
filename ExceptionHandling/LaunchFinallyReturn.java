2d9efacb9a8b89b21be07ec37f1a7ade01a59228
srilekha1818 <srilekhakavuri18@gmail.com>
2023-07-31 23:41:08 +0530

    commit

---
+++ ExceptionHandling/LaunchFinallyReturn.java
@@ -0,0 +1,25 @@
+package CoreJava.ExceptionHandling;
+class Demo
+{
+	int disp()
+	{
+		try
+		{
+		System.out.println("Method started");
+		return 10;
+		}
+		finally {
+		System.out.println("Method ending");
+		}
+	}
+}
+public class LaunchFinallyReturn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo d=new Demo();
+		d.disp();
+
+	}
+
+}
